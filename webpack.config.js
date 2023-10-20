21cd79d153c505dc04476cff45a8416d46f7295e
ROWEND36 <rowendduke36@gmail.com>
2023-10-20 12:07:18 +0100

    fix: missing css

--- webpack.config.js
+++ webpack.config.js
@@ -82,7 +82,9 @@ const config = {
       template: "src/index.html",
       chunks: ["index"],
     }),
-    new CopyWebpackPlugin([{ patterns: { from: "src/assets", to: "assets" } }]),
+    new CopyWebpackPlugin([
+      { patterns: [{ from: "src/assets", to: "assets" }] },
+    ]),
     new MiniCssExtractPlugin(),
   ],
 };
