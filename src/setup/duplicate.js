5fbd45a31566696c315ccb30be4770076c34dda1
ROWEND36 <rowendduke36@gmail.com>
2023-11-15 15:29:50 +0100

    feat: duplicate requests

--- src/setup/duplicate.js
+++ src/setup/duplicate.js
@@ -1,8 +1,10 @@
 const x = document.querySelectorAll(".duplicate");
 x.forEach((el) => {
-  const node = el.cloneNode();
-  node.setAttribute("aria-hidden", true);
-  node.classList.add("duplicated");
-  el.parentElement.classList.add("position-relative");
-  el.parentElement.appendChild(node);
+  el.addEventListener("load", () => {
+    const node = el.cloneNode();
+    node.setAttribute("aria-hidden", true);
+    node.classList.add("duplicated");
+    el.parentElement.classList.add("position-relative");
+    el.parentElement.appendChild(node);
+  });
 });
