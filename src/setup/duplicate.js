47969dbbafbfab516752195008b45c0792cc33fb
ROWEND36 <rowendduke36@gmail.com>
2023-11-16 05:34:58 +0100

    feat: closed for now

--- src/setup/duplicate.js
+++ src/setup/duplicate.js
@@ -1,10 +1,17 @@
 const x = document.querySelectorAll(".duplicate");
 x.forEach((el) => {
+  if (el.complete && el.naturalHeight !== 0) {
+    duplicate(el);
+  }
   el.addEventListener("load", () => {
-    const node = el.cloneNode();
-    node.setAttribute("aria-hidden", true);
-    node.classList.add("duplicated");
-    el.parentElement.classList.add("position-relative");
-    el.parentElement.appendChild(node);
+    duplicate(el);
   });
 });
+
+function duplicate(el) {
+  const node = el.cloneNode();
+  node.setAttribute("aria-hidden", true);
+  node.classList.add("duplicated");
+  el.parentElement.classList.add("position-relative");
+  el.parentElement.appendChild(node);
+}
